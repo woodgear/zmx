f237f043d1317382d5f02298b745ab45f88f6eb9
cong <q1875486458@gmail.com>
2026-02-21 00:31:55 +0800

    commiter: git-sync

---
+++ zmx-call
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ~/.loadhome.sh 2>&1 > /dev/null
+source ~/.zmx/import.sh 2>&1 >/dev/null
+eval "$@"
--- zmx-call.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-source ~/.loadhome.sh 2>&1 > /dev/null
-source ~/sm/project/zmx/zmx.plugin.zsh 2>&1 >/dev/null
-zmx-load-shell-actions 2>&1 >/dev/null
-eval "$@"
